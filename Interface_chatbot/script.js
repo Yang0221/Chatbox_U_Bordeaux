f19b0d0ae168d2b688a391730fd0f967b0db713a
TomimaRa <tomimahariniaina@gmail.com>
2020-02-14 17:58:39 +0100

    Amelioration

---
+++ Interface_chatbot/script.js
@@ -0,0 +1,7 @@
+function closeChat(){
+    document.getElementById('ChatBot_container').style.display = 'none';
+}
+
+function openChat(){
+    document.getElementById('ChatBot_closed').style.display = 'block';
+}
