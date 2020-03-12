a17bf562b0d720ffaebf9ba9bf264c8b926dfd5f
TomimaRa <tomimahariniaina@gmail.com>
2020-03-12 10:24:45 +0100

    Installation Django

---
+++ botEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/autofs/unitytravail/travail/tralijaona/s6/chatBot/bot-localisation/botEnv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
