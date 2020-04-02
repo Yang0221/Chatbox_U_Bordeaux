f4128feecdee84d9df52d93e61bbe7ab17ef357f
jtilatti <julie.tilatti@etu.u-bordeaux.fr>
2020-04-02 18:55:09 +0200

    modification batiment + ajout alias +ajout salle

--- managementTool/managementTool/views.py
+++ managementTool/managementTool/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from tool.models import Building
 from tool.models import Room
 from tool.models import Synonym
+from django.http import HttpResponseRedirect
 
 def index(request):
     return render(request , 'index.html', {'buildings' : Building.objects.all()})
@@ -11,6 +12,31 @@ def edit(request,id):
     rooms = Room.objects.filter(id_building = id)
     synonyms = Synonym.objects.filter(id_building = id)
     return render(request , 'edit.html', {'building' : building[0], 'rooms' : rooms, 'synonyms' : synonyms})
+
+def edit_building(request,id):
+    if request.POST:
+        building = Building.objects.filter(id = id)
+        building = building[0]
+        building.name = request.POST.get('name')
+        building.main_activity = request.POST.get('mainActivity')
+        building.address = request.POST.get('address')
+        building.coordinates = request.POST.get('coordinatesGPS')
+        building.save()
+    return HttpResponseRedirect("/edit/" + id)
+
+def add_synonym(request,id):
+    if request.POST:
+        building = Building.objects.filter(id = id)
+        new_synomym = Synonym(id_building = building[0] , value = request.POST.get('alias'))
+        new_synomym.save()
+    return HttpResponseRedirect("/edit/" + id)
+
+def add_room(request,id):
+    if request.POST:
+        building = Building.objects.filter(id = id)
+        new_room = Room(id_building = building[0] , name = request.POST.get('roomName'), floor = request.POST.get('stage'), activity = request.POST.get('activity'))
+        new_room.save()
+    return HttpResponseRedirect("/edit/" + id)
 
 def error(request):
     return index(request)
