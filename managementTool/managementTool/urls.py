5429d248b2a630956b1f9fba91b0fae07673ad86
jtilatti <julie.tilatti@etu.u-bordeaux.fr>
2020-04-27 02:00:11 +0200

    outil de gestion : suite

--- managementTool/managementTool/urls.py
+++ managementTool/managementTool/urls.py
@@ -8,10 +8,23 @@ urlpatterns = [
     path('edit/building/<id>/edit_informations' , views.edit_building_informations , name = 'edit_building_informations'),
     path('edit/building/<id>/add_synonym' , views.add_synonym_building , name = 'add_synonym_building'),
     path('edit/building/<id>/add_room' , views.add_room_building , name = 'add_room_building'),
+    #path('edit/building/<id>/edit_alias' , views.edit_alias_building , name = 'edit_alias_building'),
+    path('edit/building/<id>/delete_synonym/<alias>' , views.delete_alias_building , name = 'delete_alias_building'),
+    path('edit/building/<parent>/delete/<id>' , views.delete_room , name = 'delete_room'),
 
+    path('edit/campus/<id>' , views.edit_campus , name = 'edit_campus'),
+    path('edit/campus/<id>/edit_informations' , views.edit_campus_informations , name = 'edit_campus_informations'),
+    path('edit/campus/<id>/add_synonym' , views.add_synonym_campus , name = 'add_synonym_campus'),
+    path('edit/campus/<id>/add_building' , views.add_building_campus , name = 'add_building_campus'),
+    #path('edit/campus/<id>/edit_alias' , views.edit_alias_campus , name = 'edit_alias_campus'),
+    path('edit/campus/<id>/delete_synonym/<alias>' , views.delete_alias_campus , name = 'delete_alias_campus'),
 
-
-
+    path('edit/room/<id>' , views.edit_room , name = 'edit_room'),
+    path('edit/room/<id>/edit_informations' , views.edit_room_informations , name = 'edit_room_informations'),
+    path('edit/room/<id>/add_synonym' , views.add_synonym_room , name = 'add_synonym_room'),
+    #path('edit/room/<id>/edit_alias' , views.edit_alias_room , name = 'edit_alias_room'),
+    path('edit/room/<id>/delete_synonym/<alias>' , views.delete_alias_room , name = 'delete_alias_room'),
+    path('edit/room/<id>/delete' , views.delete_room , name = 'delete_room'),
 
     path('deleteBuilding' , views.deleteBuilding , name = 'deleteBuilding'),
     path('addItem' , views.addItem , name = 'addItem'),
--- managementTool/managementTool/views.py
+++ managementTool/managementTool/views.py
@@ -33,19 +33,72 @@ def edit_building(request,id):
     building = Building.objects.get(id = id)
     rooms = Room.objects.filter(id_building = id)
     synonyms = SynonymBuilding.objects.filter(id_building = id)
-    return render(request , 'edit.html', {'type':'building', 'building' : building, 'rooms' : rooms, 'synonyms' : synonyms})
+    campus = Campus.objects.all()
+    for c in campus:
+        if c.id == building.id_campus.id:
+            c.selected = "selected"
+        else :
+            c.selected = ""
+    return render(request , 'edit.html', {'type':'building', 'building' : building, 'rooms' : rooms, 'synonyms' : synonyms, 'campus' : campus})
 
+def edit_room(request,id):
+    room = Room.objects.get(id = id)
+    buildings = Building.objects.all()
+    for b in buildings:
+        if b.id == room.id_building.id :
+            b.selected = "selected"
+        else :
+            b.selected = ""
+    synonyms = SynonymRoom.objects.filter(id_room = id)
+    return render(request , 'edit.html', {'type':'room', 'room' : room, 'buildings' : buildings, 'synonyms' : synonyms})
+
+def edit_campus(request,id):
+    campus = Campus.objects.get(id = id)
+    buildings = Building.objects.filter(id_campus = id)
+    synonyms = SynonymCampus.objects.filter(id_campus = id)
+    return render(request , 'edit.html', {'type':'campus', 'campus' : campus, 'buildings' : buildings, 'synonyms' : synonyms})
 
 def edit_building_informations(request,id):
     if request.POST:
         building = Building.objects.get(id = id)
-        building.name = request.POST.get('name')
-        building.main_activity = request.POST.get('mainActivity')
-        building.address = request.POST.get('address')
-        building.coordinates = request.POST.get('coordinatesGPS')
+        if request.POST.get('name') != "":
+            building.name = request.POST.get('name')
+        if request.POST.get('mainActivity') != "":
+            building.main_activity = request.POST.get('mainActivity')
+        if request.POST.get('address') != "":
+            building.address = request.POST.get('address')
+        if request.POST.get('coordinatesGPS') != "":
+            building.coordinates = request.POST.get('coordinatesGPS')
+        if request.POST.get('campus_id') != "":
+            building.id_campus = Campus.objects.get(id = request.POST.get('campus_id'))
         building.save()
     return HttpResponseRedirect("/edit/building/" + id)
 
+def edit_room_informations(request,id):
+    if request.POST:
+        room = Room.objects.get(id = id)
+        if request.POST.get('name') != "":
+            room.name = request.POST.get('name')
+        if request.POST.get('mainActivity') != "":
+            room.activity = request.POST.get('mainActivity')
+        if request.POST.get('stage') != "":
+            room.floor = request.POST.get('stage')
+        if request.POST.get('building_id') != "":
+            room.id_building = Building.objects.get(id = request.POST.get('building_id'))
+        room.save()
+    return HttpResponseRedirect("/edit/room/" + id)
+
+def edit_campus_informations(request,id):
+    if request.POST:
+        campus = Campus.objects.get(id = id)
+        if request.POST.get('name') != "":
+            campus.name = request.POST.get('name')
+        if request.POST.get('address') != "":
+            campus.address = request.POST.get('address')
+        if request.POST.get('coordinatesGPS') != "":
+            campus.coordinates = request.POST.get('coordinatesGPS')
+        campus.save()
+    return HttpResponseRedirect("/edit/campus/" + id)
 
 def add_synonym_building(request,id):
     if request.POST:
@@ -54,14 +107,54 @@ def add_synonym_building(request,id):
         new_synomym.save()
     return HttpResponseRedirect("/edit/building/" + id)
 
+def add_synonym_room(request,id):
+    if request.POST:
+        if request.POST.get('alias') != "":
+            room = Room.objects.get(id = id)
+            new_synomym = SynonymRoom(id_room = room , value = request.POST.get('alias'))
+            new_synomym.save()
+    return HttpResponseRedirect("/edit/room/" + id)
+
+def add_synonym_campus(request,id):
+    if request.POST:
+        if request.POST.get('alias') != "":
+            campus = Campus.objects.get(id = id)
+            new_synomym = SynonymCampus(id_campus = campus , value = request.POST.get('alias'))
+            new_synomym.save()
+    return HttpResponseRedirect("/edit/campus/" + id)
 
 def add_room_building(request,id):
-    if request.POST:
+    if request.POST and request.POST.get('roomName') != "":
         building = Building.objects.get(id = id)
-        new_room = Room(id_building = building , name = request.POST.get('roomName'), floor = request.POST.get('stage'), activity = request.POST.get('activity'))
+        new_room = Room(id_building = building , name = request.POST.get('roomName'), floor = request.POST.get('roomStage'), activity = request.POST.get('activity'))
         new_room.save()
     return HttpResponseRedirect("/edit/building/" + id)
 
+
+def add_building_campus(request,id):
+    if request.POST and request.POST.get('buildingName') != "":
+        campus = Campus.objects.get(id = id)
+        new_building = Building(id_campus = campus , name = request.POST.get('buildingName'), address = request.POST.get('roomAddress'), main_activity = request.POST.get('activity'))
+        new_building.save()
+    return HttpResponseRedirect("/edit/campus/" + id)
+
+
+
+def delete_alias_campus(request,id,alias):
+    SynonymCampus.objects.get(id = alias).delete()
+    return HttpResponseRedirect("/edit/campus/" + id)
+
+def delete_alias_building(request,id,alias):
+    SynonymBuilding.objects.get(id = alias).delete()
+    return HttpResponseRedirect("/edit/building/" + id)
+
+def delete_alias_room(request,id,alias):
+    SynonymRoom.objects.get(id = alias).delete()
+    return HttpResponseRedirect("/edit/room/" + id)
+
+def delete_alias_campus(request,id,alias):
+    SynonymCampus.objects.get(id = alias).delete()
+    return HttpResponseRedirect("/edit/campus/" + id)
 
 def writeCSV(index, writer, list):
     type = ["campus", "batiment", "salle"]
@@ -97,19 +190,28 @@ def exportCSV(request):
 
 def deleteBuilding(request):
     if request.POST and request.is_ajax:
-        Building.objects.get(id = request.POST.get('id')).delete()
-    return HttpResponse(json.dumps("ok"), content_type="application/json")
+        return HttpResponse(json.dumps("ok"), content_type="application/json")
+
+def delete_room(request,id, parent = 0):
+    Room.objects.get(id = id).delete()
+    if parent != 0 :
+        return HttpResponseRedirect("/edit/building/" + parent)
+    HttpResponse(request.path_info)
 
 def addItem(request):
     if request.POST and request.is_ajax:
         if request.POST.get('type') == 'building':
             new_item = Building(name = request.POST.get('new_name'))
+            new_item.save()
+            return HttpResponseRedirect("/edit/building/" + new_item.id)
         elif request.POST.get('type') == 'campus':
             new_item = Campus(name = request.POST.get('new_name'))
+            new_item.save()
+            return HttpResponseRedirect("/edit/campus/" + new_item.id)
         if request.POST.get('type') == 'room':
             new_item = Room(name = request.POST.get('new_name'))
-        new_item.save()
-    return HttpResponse(json.dumps(new_item.id), content_type="application/json")
+            new_item.save()
+            return HttpResponseRedirect("/edit/room/" + new_item.id)
 
 # def displayTable(request):
 #     informations = displayInformations()
