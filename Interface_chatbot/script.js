f141f81027a85c2b87e52ff04e3a96a58d76e22e
jtilatti <julie.tilatti@etu.u-bordeaux.fr>
2020-05-07 15:59:50 +0200

    nottyage dépot

--- Interface_chatbot/script.js
+++
@@ -1,7 +0,0 @@
-function closeChat(){
-    document.getElementById('ChatBot_container').style.display = 'none';
-}
-
-function openChat(){
-    document.getElementById('ChatBot_closed').style.display = 'block';
-}
